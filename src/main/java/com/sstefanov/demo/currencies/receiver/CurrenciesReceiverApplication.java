e670c16f4e30de234d4d8940159b4f774be2cfa2
Stefan Stefanov <elken@abv.bg>
2024-10-11 12:45:36 +0300

    refactor properties

--- src/main/java/com/sstefanov/demo/currencies/receiver/CurrenciesReceiverApplication.java
+++ src/main/java/com/sstefanov/demo/currencies/receiver/CurrenciesReceiverApplication.java
@@ -1,13 +1,16 @@
 package com.sstefanov.demo.currencies.receiver;
 
+import com.sstefanov.demo.currencies.receiver.config.ServerUrlProps;
+import com.sstefanov.demo.currencies.receiver.config.WebSocketProps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({ServerUrlProps.class, WebSocketProps.class})
 public class CurrenciesReceiverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrenciesReceiverApplication.class, args);
 	}
-
 }
--- src/main/java/com/sstefanov/demo/currencies/receiver/config/WebSocketClientConfig.java
+++ src/main/java/com/sstefanov/demo/currencies/receiver/config/WebSocketClientConfig.java
@@ -1,29 +1,26 @@
 package com.sstefanov.demo.currencies.receiver.config;
 
-import com.sstefanov.demo.currencies.receiver.services.Props;
 import com.sstefanov.demo.currencies.receiver.services.WebSocketService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
-@EnableScheduling
 @Configuration
 @Slf4j
+@RequiredArgsConstructor
+@EnableScheduling
 public class WebSocketClientConfig {
 
-    @Autowired
-    private WebSocketService webSocketService;
-
-    @Autowired
-    private Props props;
-
+    private final WebSocketService webSocketService;
+    private final ServerUrlProps serverUrlProps;
+    private final WebSocketProps webSocketProps;
     private WebSocketSession session;
 
-    @Scheduled(initialDelay = 100, fixedRate = 500)
+    @Scheduled(initialDelay = 100, fixedRate = 5500)
     public void start() throws Exception {
 
         if (session == null || !session.isOpen()) {
@@ -34,9 +31,9 @@ public class WebSocketClientConfig {
     public void init() throws Exception {
         StandardWebSocketClient client = new StandardWebSocketClient();
         try {
-            session = client.execute(webSocketService, props.getCurrencyUpdaterServerUrl()).get();
-            session.setTextMessageSizeLimit(props.getWsTextMessageSizeLimit());
-        } catch ( java.util.concurrent.ExecutionException e) {
+            session = client.execute(webSocketService, serverUrlProps.getUrl()).get();
+            session.setTextMessageSizeLimit(webSocketProps.getTextMessageSizeLimit());
+        } catch (java.util.concurrent.ExecutionException e) {
             log.error("Cannot open WS session: " + e.getCause());
         }
     }
